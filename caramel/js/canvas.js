415a13b2c641e0b1dba07712e21ac94bca3bad0d
Antsiferov Maxim <antsiferovmaximv@gmail.com>
2017-08-28 19:32:45 +0300

    Update canvas.js

--- caramel/js/canvas.js
+++ caramel/js/canvas.js
@@ -25,7 +25,7 @@ var caramel = document.createElement("img");
 caramel.onload = function() {
 	caramel_img = this;
 };
-caramel.src = "../img/caramel.png";
+caramel.src = "/caramel/img/caramel.png";
 
 function drawPoint(x, y, r) {
 	ctx.fillStyle = "#000";
