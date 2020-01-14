9de03949b77005e0debf8d9cde44ff01d5f1e578
AntsiferovMaxim <antsiferovmaximv@gmail.com>
2020-01-14 11:29:29 +0200

    .

---
+++ precache-manifest.f04ef998007487e255573b5c4748572b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3429b05b81e22b339a54d302fa63ca57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "845b7142b3c6e8ac17f6",
+    "url": "/static/css/main.66b2c93a.chunk.css"
+  },
+  {
+    "revision": "6de8f7793e70d1edc5f2",
+    "url": "/static/js/2.d3d17c22.chunk.js"
+  },
+  {
+    "revision": "1eed39b55c0bb7c26ec629a63f87aa2a",
+    "url": "/static/js/2.d3d17c22.chunk.js.LICENSE"
+  },
+  {
+    "revision": "845b7142b3c6e8ac17f6",
+    "url": "/static/js/main.8cb93f35.chunk.js"
+  },
+  {
+    "revision": "1ff311e9c24f3c2c4458",
+    "url": "/static/js/runtime-main.47618631.js"
+  }
+]);
